ca25d852188d8cca5ce06b8d89d04f01a012fe75
Yuvi <ykaushal364@gmail.com>
2022-08-02 10:22:20 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+userSchema = new Schema({
+    unique_id: Number,
+    email: String,
+    username: String,
+    password: String,
+    passwordCanf: String,
+    createdAt:{type: Date, default: Date.now},
+});
+
+User = mongoose.model('User', userSchema);
+module.exports = User;
